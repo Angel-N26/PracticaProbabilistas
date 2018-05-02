ddf8618f72ba8a87dfc0c50c0892b71308f894f1
Angel <Angel@172.19.188.1>
2018-05-02 18:39:09 +0200

    Version 0.0

---
+++ src/practicaprobabilistas/PracticaProbabilistas.java
@@ -0,0 +1,13 @@
+package practicaprobabilistas;
+
+/**
+ * @author Angel Loro, Angel Sánchez
+ **/
+
+public class PracticaProbabilistas {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
