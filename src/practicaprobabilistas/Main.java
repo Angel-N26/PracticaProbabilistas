4699b40fb033bf8928dba34fa272440cab523d3e
angelloro <Angel.Loro@alu.uclm.es>
2018-05-07 02:46:07 +0200

    version 0.9 menu

--- src/practicaprobabilistas/Main.java
+++ src/practicaprobabilistas/Main.java
@@ -8,14 +8,38 @@ import utilidades.leer;
 
 public class Main {
 
-    public static void main(String [] args){        
-        PracticaProbabilistas pp = new PracticaProbabilistas();        
-        int k = leer.entero("Numero de veces: ");	
-        PracticaProbabilistas pp2 = new PracticaProbabilistas(3,2,2,2);      
-        String ss ="Volumen: " + pp.volumen(k) + " Intervalo de confianza[" + pp.getIntConfPos(0) + " , " + pp.getIntConfPos(1) + "]" + 
-                "V: " + pp2.volumenEsfera(k);
-        leer.pln(ss);
+    public static void main(String[] args) {
+        menu();
 
     }
+    public static void menu(){
+    
+        boolean seguir = true;
+        int k ;
+        do {
+            switch (leer.entero("Indique la opcion que desea realizar:\n\t1)x^2 + y^2 = z\n\t2)x^xP + y^yP + z^zP = r^2\n\t3)Salir\n")) {
+                //x^2 + y^2 = z
+                case 1:
+                    PracticaProbabilistas pp = new PracticaProbabilistas();
+                    k = leer.entero("Numero de veces: ");
+                    leer.pln("Volumen: " + pp.volumen(k) + " Intervalo de confianza[" + pp.getIntConfPos(0) + " , " + pp.getIntConfPos(1) + "]");
+                    break;
+                // x^i + y^j+ z^k=r^2
+                case 2:
+                    PracticaProbabilistas pe = new PracticaProbabilistas(leer.real("radio:\n"),leer.real("xP:\n"),leer.real("yP:\n"),leer.real("zP:\n"));
+                    k = leer.entero("Numero de veces: ");
+                    leer.pln("Volumen: " + pe.volumen(k));
+                    break;
 
+                case 3:
+                    seguir = false;
+                    break;
+
+                default:
+                    leer.pln("Opcion invalida");
+
+            }
+
+        } while (seguir);
+    }
 }
