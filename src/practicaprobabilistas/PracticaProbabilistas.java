a1c5294c1b53f6cd6bd9ad992c021a5b7daca28e
Angel <Angel@172.19.188.1>
2018-05-07 02:19:11 +0200

    Version 0.7 (Mirar y hacer main)

--- src/practicaprobabilistas/PracticaProbabilistas.java
+++ src/practicaprobabilistas/PracticaProbabilistas.java
@@ -1,54 +1,71 @@
 package practicaprobabilistas;
 
-import utilidades.leer;
-
 /**
  * @author Angel Loro, Angel Sánchez
  **/
 
 public class PracticaProbabilistas {
-
-    double r, cX, cY, linf, lsup; //radio y centro (cX , cY)
+    
+    double radio;
+    double intervaloConf [];
+    
+    public PracticaProbabilistas() {
+        
+    }
     
     public PracticaProbabilistas(double radio) {
-        linf = centroX - radio;
-        lsup = centroX + radio;
-        r = radio;
-        cX = centroX;
-        cY = centroY;
+        this.radio = radio;
     } 
     
-    public double areaNumericoP(int k, double [] intervaloConf) { //area probabilista lanzando k puntos A = casos favorables / casos posibles         
-        //PracticaProbabilistas pp = new PracticaProbabilistas(r,0.0,0.0);
-        int buenos = 0;
-        for (int n = 0; n < k; n++) {
-            double x = Math.random() * r;
-            double y = Math.random() * r;
-            if (y <= f(x)) {
-                buenos++;
-            }
-        }
-        intervaloConf = IntervaloConfProporciones((double) buenos / k, k);
-        return 4 * r * r * (double) buenos / k; //buenos/k proporcion;r * r total del area 4* porque es solo un cuadrante
+    public double getIntConfPos(int n){
+        return intervaloConf[n];
+    }
+    
+    public double volumen(int k) { //area probabilista lanzando k puntos A = casos favorables / casos posibles         
+        double[] values = new double[k];         
+        double suma = 0.0;
+        for (int i = 0; i < k; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            double z = f(x,y);            
+            suma += (3.14/4)*z; // Area de la Base por la altura // 1/4 del area de la circunferencia, 4pi/4
+            values[i] = (3.14/4)*z;
+        }               
+        intervaloConf = IntervaloConfProporciones(values);        
+        return suma / k; //Teorema de la media
+    }
+    
+    public double volumenEsfera(int k) {
+        double dentro = 0, fuera = 0;        
+        double vol_prisma;
+        for(int i = 0 ; i < k ; i++){                
+            double x = Math.random() * radio;
+            double y = Math.random() * radio;
+            double z = Math.random() * radio;
+            // Si el punto esta dentro de la esfera, se incrementa el contador
+            if ((x * x + y * y + z * z) <= radio * radio) {
+                dentro++;
+            } else {
+                fuera++;
+            }            
+        }         
+        // V = AreaBase * Altura (Siendo la altura el diametro: 2r)
+        vol_prisma = Math.pow(radio, 3);
+        // Resultado = (Numero de puntos dentro / puntos totales) * volumen del prisma que lo contiene               
+        return 8 * ((dentro / (dentro + fuera)) * vol_prisma); // Se multiplica por 8 para tener en cuenta la esfera completa.
     }
     
     public double f(double x, double y) {
-        return Math.pow(x, x_pow) + Math.pow(y, y_pow);
-    }
+        return (x * x) + (y * y);
+    }    
     
-    public boolean vale(double x) {
-        boolean v = x >= linf && x <= lsup;
-        if (!v) {
-            leer.pln(x + " no pertenece a " + this);
-        }
-        return v;
-    }
-    
-    public double[] IntervaloConfProporciones(double p, int n) {
-        double[] intervaloP = new double[2];
-        System.out.println("p = " + p);
-        intervaloP[0] = p - 1.96 * Math.sqrt(p * (1 - p) / n);
-        intervaloP[1] = p + 1.96 * Math.sqrt(p * (1 - p) / n);
+    public double [] IntervaloConfProporciones(double[] valores) {
+        double[] intervaloP = new double[2];   
+        double media = media(valores);
+        double cuasiV = cuasiV(valores, media);
+        
+        intervaloP[0] = media - 1.96 * cuasiV/Math.sqrt(valores.length);
+        intervaloP[1] = media + 1.96 * cuasiV/Math.sqrt(valores.length);
         return intervaloP;
     }
     
